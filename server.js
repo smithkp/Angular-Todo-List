7e208b9933015207aa544b0a16e82250de39decd
Kyle <kyles6688@gmail.com>
2019-06-02 01:47:31 -0400

    changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/dist/shopping-list'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/shopping-list/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
