f49d7772e59f534337c214b6d0f3e8328e09b2fe
Kyle <kyles6688@gmail.com>
2019-06-01 20:49:47 -0400

    server.js change

--- src/server.js
+++ src/server.js
@@ -4,12 +4,11 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/shopping-list'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/shopping-list/index.html'));
 });
-
 
 app.listen(process.env.PORT || 8080);
